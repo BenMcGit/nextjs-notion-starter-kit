6e1f498327a9c4bbea6471d188bf1ab825b8f253
Ben McAdams <mcadams.benj@gmail.com>
2025-02-17 16:24:41 -0800

    update site config

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: 'AI Insights Daily',
+  domain: 'aiinsightsdaily.com',
   author: 'Ben McAdams',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'All the AI insights you need to stay ahead of the curve',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'benwmcadams',
+  github: 'benmcgit',
+  linkedin: 'benmcgit',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
